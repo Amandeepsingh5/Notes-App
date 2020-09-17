f67e340377868c6417e066895ff721fde8b8cbef
amandeepsingh5 <amangartan7@hotmail.com>
2020-09-17 15:35:50 -0400

    Added shownotes function

--- js/app.js
+++ js/app.js
@@ -15,5 +15,42 @@ addBtn.addEventListener("click", function(e) {
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
 //   console.log(notesObj);
-
+showNotes();
 });
+
+function showNotes(){
+
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+      notesObj = [];
+    } else {
+      notesObj = JSON.parse(notes);
+    }
+
+    let html = "";
+    notesObj.forEach(function(element,index){
+
+        html += `
+        
+        <div id="notes" class="row container-fluid">
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Note ${index + 1}</h5>
+                  <p class="card-text"> ${element}</p>
+                  <button href="#" class="btn btn-primary">Delete Note</button>
+                </div>
+              </div>    
+        
+        `
+
+
+    });
+
+    let notesElm = document.getElementById('notes');
+    if(notes.length !=0){
+
+
+        notesElm.innerHTML = html;
+    }
+
+}
